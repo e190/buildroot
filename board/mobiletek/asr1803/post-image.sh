c56fa4d0ccde3478e27a8cebcbc5d4a449b775e1
sniper <e190@163.com>
2022-06-09 11:27:37 +0800

    add helloworld package.

--- board/mobiletek/asr1803/post-image.sh
+++ board/mobiletek/asr1803/post-image.sh
@@ -7,8 +7,16 @@ red='\e[0;31m' # 红色
 green='\e[0;32m' # 绿色
 NC='\e[0m' # 没有颜色
 
+function kernel-image ()
+{
+	cp ${BINARIES_DIR}/zImage ${BINARIES_DIR}/openwrt-mmp-asr1803-zImage
+	cat ${BUILD_DIR}/linux/arch/arm/boot/dts/asr1803-p401.dtb >> ${BINARIES_DIR}/openwrt-mmp-asr1803-zImage
+	# cat ${BUILD_DIR}/linux/arch/arm/boot/dts/asr1803-ab-p401.dtb >> ${BINARIES_DIR}/openwrt-mmp-asr1803-zImage
+}
+
 main()
 {
+    kernel-image
     cp ${BINARIES_DIR}/rootfs.ubi ${BINARIES_DIR}/buildroot.ubi
 
     echo -e "${green}************************************ ${NC}"
